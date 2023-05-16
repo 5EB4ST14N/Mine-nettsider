341ca1b0916bc3634e9ffe6c080153db2d314ac8
5EB4ST14N <113419631+5EB4ST14N@users.noreply.github.com>
2023-05-16 10:07:07 +0200

    Add files via upload

---
+++ div/db.js
@@ -0,0 +1,8 @@
+let russeKort = {
+    "personer": [
+        { "navn": "Børre", "skole": "Kirkenes VGS", "adr": "adr: Esso", "tlf": "tlf: Stasjonær", "sitat1": "Slayyy", "sitat2": "Queeen" },
+        { "navn": "Arnfinn", "skole": "Hallingdal VGS", "adr": "adr: Elgenes rike", "tlf": "tlf: Naturen ringer", "sitat1": "Elg purrr", "sitat2": "#moosenbooze" },
+        { "navn": "Albert", "skole": "Longyearbyen VGS", "adr": "adr:Isbjørn", "tlf": "tlf: Send med due", "sitat1": "Kødd me mæ? Ikke faen", "sitat2": "#duedø" },
+        { "navn": "Karl", "skole": "Fredrikstad", "adr": "adr: Onkel Ronny", "tlf": "tlf: Hjertrud skrikær", "sitat1": "brylupp med kusine turid 24.08.2023", "sitat2": "#familyfirst" },
+    ],
+}
